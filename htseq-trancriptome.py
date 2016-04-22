08124ff4046cc57ca292c69990cb05bb958a93fa
oxpeter <oxpeter+git@gmail.com>
2016-04-22 15:07:52 -0400

    Cleaned up file and added comments

--- htseq-trancriptome.py
+++ htseq-trancriptome.py
@@ -1,33 +1,13 @@
-# coding: utf-8
-import HTSeq as hs
-import os
-samfile = 'Cplan_Q2_16Aligned.out.sam'
-gtffile = '/home/antqueen/genomics/experiments/analyses/PRO20160405_camponotus/trinity_denovo_normalized_camponotus/Transdecoder_ss/merge_genesets/Cpla_td_gff.Apr21_11.15.families.gtf'
-gtf = hs.GFF_Reader(gtffile)
-import itertools
-exons = hs.GenomicArrayOfSets( "auto", stranded=True)
+#!/usr/bin/env python
+
+import argparse
 import collections
-sam_file = hs.SAM_Reader(samfile)
-counts = collections.Counter( )
-for almnt in itertools.islice( sam_file, 100):
-    if not almnt.aligned:
-        count[ "_unmapped" ] += 1
-        continue
-    gene_ids = set()
-    for iv, val in exons[ almnt.iv ].steps():
-        gene_ids |= val
-    if len(gene_ids) == 1:
-        gene_id = list(gene_ids)[0]
-        counts[ gene_id ] += 1
-    elif len(gene_ids) == 0:
-        counts[ "_no_feature" ] += 1
-    else:
-        counts[ "_ambiguous" ] += 1
-        
-print counts
-for g, c in counts.items():
-    print "%-10s %d" % (g, c)
-    
+import itertools
+import os
+
+import HTSeq as hts
+
+
 def assess_bundle(bundle, features):
     counts = collections.Counter()
     for almnt in bundle:
@@ -44,4 +24,42 @@ def assess_bundle(bundle, features):
         else:
             counts[ "_ambiguous" ] += 1
     return counts
-multi_iterator = hs.pair_SAM_alignments( sam_file , bundle=True)
+
+# define some test files:
+samfile = '/home/antqueen/booster/PRO_Odontomachus/trinity_denovo_normalized_camponotus/Star/Cplan_Q2_16Aligned.out.sam'
+gtffile = '/home/antqueen/genomics/experiments/analyses/PRO20160405_camponotus/trinity_denovo_normalized_camponotus/Transdecoder_ss/merge_genesets/Cpla_td_gff.Apr21_11.15.families.gtf'
+
+# create gtf iterator
+gtf = hts.GFF_Reader(gtffile)
+
+# create genomic array and populate with exon features (transcripts and genes)
+exons = hts.GenomicArrayOfSets( "auto", stranded=True)
+for feature in gtf:
+    if feature.type == "exon":
+        exons[ feature.iv ] += feature.name
+
+# create Reader class for samfile:
+sam_file = hts.SAM_Reader(samfile)
+
+# classic alignment counter for ungapped single end reads:
+counts = collections.Counter( )
+for almnt in itertools.islice( sam_file, 100):
+    if not almnt.aligned:
+        count[ "_unmapped" ] += 1
+        continue
+    gene_ids = set()
+    for iv, val in exons[ almnt.iv ].steps():
+        gene_ids |= val
+    if len(gene_ids) == 1:
+        gene_id = list(gene_ids)[0]
+        counts[ gene_id ] += 1
+    elif len(gene_ids) == 0:
+        counts[ "_no_feature" ] += 1
+    else:
+        counts[ "_ambiguous" ] += 1
+
+for g, c in counts.items():
+    print "%-10s %d" % (g, c)
+
+
+read_pair_bundles = hts.pair_SAM_alignments( sam_file , bundle=True)
