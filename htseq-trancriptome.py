f72e02fe97d91339d2bd8d348392f89202f154f0
oxpeter <oxpeter+git@gmail.com>
2016-04-25 13:56:39 -0400

    added command line and progress display
    command options added through argparser, to match all options
    available to HTSeq-count. Some of the options are not yet enabled
    (in that they can be selected, but don't do anything).
    
    Also added for the PE counter an output for every 1 000 000
    bundles.

--- htseq-trancriptome.py
+++ htseq-trancriptome.py
@@ -2,7 +2,9 @@
 
 import argparse
 import collections
+import datetime
 import itertools
+import logging
 import os
 import sys
 
@@ -39,12 +41,12 @@ def assess_bundle(bundle, features):
             counts[ "_ambiguous" ] += 1
     return counts
 
-def ungapped_se_counter_demo(sam_reader, feature_array):
+def ungapped_se_counter(sam_reader, feature_array):
     """
     classic alignment counter for ungapped single end reads
     """
     counts = collections.Counter( )
-    for almnt in itertools.islice( sam_reader, 100):
+    for almnt in sam_reader:
         if not almnt.aligned:
             count[ "_unmapped" ] += 1
             continue
@@ -61,11 +63,20 @@ def ungapped_se_counter_demo(sam_reader, feature_array):
 
     return counts
 
-def ungapped_pe_counter_demo(sam_reader, feature_array):
+def ungapped_pe_counter(sam_reader, feature_array):
     counts = collections.Counter( )
     pair_iterator = hts.pair_SAM_alignments( sam_reader, bundle=True )
     # bundle puts all multiply-mapped pairs together.
-    for bundle in itertools.islice( pair_iterator, 100 ):
+
+    t0 = datetime.datetime.now()
+    for ic, bundle in enumerate(pair_iterator):
+
+        # report progress (to prove that it is still alive):
+        if ic % 1000000 == 0:
+            t1 = datetime.datetime.now()
+            print "\r%d read bundles counted in %s\r" % (ic, t1-t0)
+            sys.stdout.flush()
+
         bcounts = assess_bundle(bundle, feature_array)
 
         """
@@ -95,37 +106,137 @@ def ungapped_pe_counter_demo(sam_reader, feature_array):
 
     return counts
 
+
+parser = argparse.ArgumentParser(description=
+        """An HTSeq script to count paired-end reads that multiply map, for making
+        inferences about gene counts in a transcriptome assembly""")
+### input options ###
+parser.add_argument("-o", "--output", type=str, default='htseq_transcriptome',
+                    help="""specify the filename base to save results to [default is
+                    'htseq_transcriptome_counts'""")
+parser.add_argument("-d", "--directory", type=str, default=os.getcwd(),
+                    help="""specify the directory to save results to [default is current
+                    working directory""")
+
+
+# data file options:
+parser.add_argument('alignment_file', nargs=1, type=str,
+                    help="""The <alignment_file> contains the aligned reads in the SAM
+                    format. (Note that the SAMtools contain Perl scripts to convert most
+                    alignment formats to SAM.)""")
+parser.add_argument('gtf_file', nargs=1, type=str,
+                    help="""The <gtf_file> contains the features in the GFF format.""")
+
+parser.add_argument("-f", "--format", type=str, choices=['sam', 'bam'],
+                    help="""Format of the input data. Possible values are sam
+                    (for text SAM files) or bam (for binary BAM files). Default is sam""")
+parser.add_argument("-r", "--order", type=str,
+                    help="""
+                    For paired-end data, the alignment have to be sorted either by
+                    read name or by alignment position. If your data is not sorted, use
+                    the samtools sort function of samtools to sort it. Use this option,
+                    with name or pos for <order> to indicate how the input data has been
+                    sorted. The default is name.
+
+                    If name is indicated, htseq-count expects all the alignments for the
+                    reads of a given read pair to appear in adjacent records in the input
+                    data. For pos, this is not expected; rather, read alignments whose
+                    mate alignment have not yet been seen are kept in a buffer in memory
+                    until the mate is found. While, strictly speaking, the latter will
+                    also work with unsorted data, sorting ensures that most alignment
+                    mates appear close to each other in the data and hence the buffer
+                    is much less likely to overflow.""")
+parser.add_argument("-s", "--stranded", choices=['yes', 'no', 'reverse'], type=str,
+                    default="yes",
+                    help="""
+                    whether the data is from a strand-specific assay (default: yes)
+
+                    For stranded=no, a read is considered overlapping with a feature
+                    regardless of whether it is mapped to the same or the opposite strand
+                    as the feature. For stranded=yes and single-end reads, the read has to
+                    be mapped to the same strand as the feature. For paired-end reads,
+                    the first read has to be on the same strand and the second read on
+                    the opposite strand. For stranded=reverse, these rules are reversed.
+                    """)
+parser.add_argument("-t", "--type", type=str, default='exon',
+                    help="""feature type (3rd column in GFF file) to be used, all features
+                    of other type are ignored (default, suitable for RNA-Seq analysis
+                    using an Ensembl GTF file: exon)""")
+parser.add_argument("-i", "--idattr", type=str, default='gene_id',
+                    help="""GFF attribute to be used as feature ID. Several GFF lines with
+                    the same feature ID will be considered as parts of the same feature.
+                    The feature ID is used to identity the counts in the output table.
+                    The default, suitable for RNA-Seq analysis using an Ensembl GTF file,
+                    is gene_id.""")
+parser.add_argument("-y", "--read_type", choices=['single_end', 'paired_end'], type=str,
+                    default="paired_end",
+                    help="""
+                    whether the data is from a single-end read library, or a paired-end
+                    library. Default is paired-end.
+                    """)
+args = parser.parse_args()
+
+print args
+
 # define some test files:
 samfile = '/home/antqueen/booster/PRO_Odontomachus/trinity_denovo_normalized_camponotus/Star/Cplan_Q2_16Aligned.out.sam'
 gtffile = '/home/antqueen/genomics/experiments/analyses/PRO20160405_camponotus/trinity_denovo_normalized_camponotus/Transdecoder_ss/merge_genesets/Cpla_td_gff.Apr21_11.15.families.gtf'
 
 # create gtf iterator
-print "Reading gtf file...",
-gtf = hts.GFF_Reader(gtffile)
+print "\nReading gtf file %s..." % (args.gtf_file[0]),
+gtf = hts.GFF_Reader(args.gtf_file[0])
 print " done."
 
 # create genomic array and populate with exon features (transcripts and genes)
 print "Populating genomic array with GTF features...",
 sys.stdout.flush()
-exons = hts.GenomicArrayOfSets( "auto", stranded=True)
+
+if args.stranded == 'yes':
+    feature_array = hts.GenomicArrayOfSets( "auto", stranded=True)
+elif args.stranded == 'no':
+    feature_array = hts.GenomicArrayOfSets( "auto", stranded=False)
+
 for feature in gtf:
-    if feature.type == "exon":
-        exons[ feature.iv ] += feature.name
-print "done."
+    if feature.type == args.type:
+        feature_array[ feature.iv ] += feature.name
+
+print "done.\n\n"
 
 # create Reader class for samfile:
-sam_file = hts.SAM_Reader(samfile)
+if args.format == 'sam':
+    alnmt_file = hts.SAM_Reader(args.alignment_file[0])
+else:
+    alnmt_file = hts.BAM_Reader(args.alignment_file[0])
 
-# get counts assuming ungapped SE reads :
-counts = ungapped_se_counter_demo(sam_file, exons)
-print "\nUngapped SE counts:"
-for g, c in sorted(counts.items()):
-    print "%-10s %d" % (g, c)
 
-# get counts assuming ungapped PE reads :
-counts = ungapped_pe_counter_demo(sam_file, exons)
+# count reads:
+print "Counting reads..."
 
-print "\nUngapped PE counts:"
-for g, c in sorted(counts.items()):
-    print "%-10s %d" % (g, c)
+if args.read_type == 'single_end':
+    counts = ungapped_se_counter(alnmt_file, feature_array)
+
+    print "\nSample output for ungapped SE counts:"
+    countlist = sorted(counts.items())
+    for g, c in countlist[-10:]:
+        print "%-10s %d" % (g, c)
+else:
+    counts = ungapped_pe_counter(alnmt_file, feature_array)
+
+    print "\nSample output for ungapped PE counts:"
+    countlist = sorted(counts.items())
+    for g, c in countlist[-10:]:
+        print "%-10s %d" % (g, c)
+
+
+# output counts to file:
+print "Saving gene counts to file...",
+sys.stdout.flush()
+outfile = args.output + "_counts.txt"
+handle = open(outfile, 'w')
+for g, c in countlist:
+    handle.write("%-20s %d\n" % (g, c))
+handle.close()
+print "done."
+
+print "All done."
 
